1800ed6c1d230aa861c0bd511ffd6a38a6fd28b9
Donia Salah <doniasalah954@gmail.com>
2025-08-10 22:34:56 +0300

    Task 1: Setup project with custom

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# ✅ عرض كل الكتب
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # متاح للجميع
+
+# ✅ عرض كتاب واحد
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # متاح للجميع
+
+# ✅ إنشاء كتاب جديد
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # لازم تسجيل دخول
+
+# ✅ تعديل كتاب
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # لازم تسجيل دخول
+
+# ✅ حذف كتاب
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # لازم تسجيل دخول
