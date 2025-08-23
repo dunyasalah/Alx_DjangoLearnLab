502fda2fc8b6cf6a79ab609c3dab2789306c37fc
Donia Salah <doniasalah954@gmail.com>
2025-08-23 20:44:07 +0300

    Add user follow and feed functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,31 +1,19 @@
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404
-from .models import CustomUser
-from .models import Post
-from .serializers import PostSerializer
+from accounts.models import CustomUser
 
-class FollowUserView(APIView):
-    permission_classes = [IsAuthenticated]
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        user_to_follow = CustomUser.objects.get(id=user_id)
         request.user.following.add(user_to_follow)
-        return Response({"message": f"You are now following {user_to_follow.username}."})
+        return Response({"status": "followed"})
 
-class UnfollowUserView(APIView):
-    permission_classes = [IsAuthenticated]
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        user_to_unfollow = CustomUser.objects.get(id=user_id)
         request.user.following.remove(user_to_unfollow)
-        return Response({"message": f"You have unfollowed {user_to_unfollow.username}."})
-class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user = request.user
-        posts = Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        return Response({"status": "unfollowed"})
