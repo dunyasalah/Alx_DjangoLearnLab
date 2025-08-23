4c128d918616e18e95f4aa722ed2fec64633f349
Donia Salah <doniasalah954@gmail.com>
2025-08-23 20:47:50 +0300

    views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics, permissions
+from rest_framework.response import Response
 from posts.models import Post
-from posts.serializers import PostSerializer
 
-class FeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
+class FeedView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        return Post.objects.filter(author__in=self.request.user.following.all()).order_by('-created_at')
+    def get(self, request):
+        following_users = request.user.following.all()
+        feed_posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # ده السطر المطلوب
+        data = [{"id": post.id, "title": post.title, "content": post.content, "author": post.author.username, "created_at": post.created_at} for post in feed_posts]
+        return Response(data)
