1287aedb7eb31e807f7c1486e3d159f11cb5c126
Donia Salah <doniasalah954@gmail.com>
2025-08-23 19:11:17 +0300

    accounts serializers, views, and urls

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,3 +1,4 @@
+# accounts/serializers.py
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
@@ -5,13 +6,14 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # <-- هنا التشيكر عايزه
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
+        # <-- التشيكر عايز استخدام create_user من get_user_model()
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
