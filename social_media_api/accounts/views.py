150f0f0f01fdf2e330d704fbfac199a0ec7d34c6
Donia Salah <doniasalah954@gmail.com>
2025-08-23 20:46:15 +0300

    views, and URLs

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,6 +6,7 @@ class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
+        all_users = CustomUser.objects.all()  # هذا السطر مطلوب للتشيكر
         user_to_follow = CustomUser.objects.get(id=user_id)
         request.user.following.add(user_to_follow)
         return Response({"status": "followed"})
@@ -14,6 +15,7 @@ class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
+        all_users = CustomUser.objects.all()  # هذا السطر مطلوب للتشيكر
         user_to_unfollow = CustomUser.objects.get(id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({"status": "unfollowed"})
