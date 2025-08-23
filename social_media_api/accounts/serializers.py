2826f2e978c28530ba4df4336b5468da43cd122d
Donia Salah <doniasalah954@gmail.com>
2025-08-23 19:08:42 +0300

    accounts serializers, views, and urls

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,7 +9,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
         user = User.objects.create_user(
