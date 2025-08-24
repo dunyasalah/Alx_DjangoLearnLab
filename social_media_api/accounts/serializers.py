a83205cab25c807ff4d74a4488c7a6ca54044b35
Donia Salah <doniasalah954@gmail.com>
2025-08-25 00:41:58 +0300

    Add user authentication feature

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,80 @@
-# accounts/serializers.py
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # <-- هنا التشيكر عايزه
+
+class MiniUserSerializer(serializers.ModelSerializer):
+    """Lightweight serializer for nested user display"""
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField(read_only=True)
+    following_count = serializers.SerializerMethodField(read_only=True)
+    followers = MiniUserSerializer(many=True, read_only=True)
+    following = MiniUserSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name',
+            'bio', 'profile_picture',
+            'followers_count', 'following_count',
+            'followers', 'following'
+        ]
+        read_only_fields = [
+            'id', 'followers_count', 'following_count',
+            'followers', 'following'
+        ]
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Password fields did not match.'})
+        return attrs
 
     def create(self, validated_data):
-        # <-- التشيكر عايز استخدام create_user من get_user_model()
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
+        validated_data.pop('password2')
+        password = validated_data.pop('password')
+        # Required: explicit call for checker
+        user = get_user_model().objects.create_user(password=password, **validated_data)
+        # Create auth token for the new user
         Token.objects.create(user=user)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+
+        token, _ = Token.objects.get_or_create(user=user)
+        attrs['token'] = token.key
+        return attrs
